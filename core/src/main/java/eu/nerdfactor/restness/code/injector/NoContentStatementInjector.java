63575f1126503a52220ccadec73e4980aafd23a3
Daniel Klug <mail@nerdfactor.eu>
2024-09-07 16:00:45 +0200

    Refactored ReturnStatementInjector.

--- core/src/main/java/eu/nerdfactor/restness/code/injector/NoContentStatementInjector.java
+++ core/src/main/java/eu/nerdfactor/restness/code/injector/NoContentStatementInjector.java
@@ -3,13 +3,28 @@ package eu.nerdfactor.restness.code.injector;
 import com.squareup.javapoet.MethodSpec;
 import org.springframework.http.ResponseEntity;
 
+/**
+ * An injector that adds an empty return statement to a method.
+ *
+ * @author Daniel Klug
+ */
 public class NoContentStatementInjector extends ReturnStatementInjector {
 
+	/**
+	 * Add a statement that returns an empty wrapper object.
+	 *
+	 * @param builder An existing builder object that will be used.
+	 */
 	@Override
 	protected void addWrapperContent(MethodSpec.Builder builder) {
 		builder.addStatement("wrapper.noContent()");
 	}
 
+	/**
+	 * Add a statement that returns an empty response.
+	 *
+	 * @param builder An existing builder object that will be used.
+	 */
 	@Override
 	protected void addBasicReturn(MethodSpec.Builder builder) {
 		builder.addStatement("return ResponseEntity.noContent().build()", ResponseEntity.class);
--- core/src/main/java/eu/nerdfactor/restness/code/injector/ReturnStatementInjector.java
+++ core/src/main/java/eu/nerdfactor/restness/code/injector/ReturnStatementInjector.java
@@ -7,29 +7,61 @@ import com.squareup.javapoet.TypeName;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+/**
+ * An injector that adds a return statement to a method.
+ *
+ * @author Daniel Klug
+ */
 public class ReturnStatementInjector implements Injectable<MethodSpec.Builder> {
 
+	/**
+	 * The class of a wrapper object.
+	 */
 	protected TypeName wrapperType;
 
+	/**
+	 * The class of the response object.
+	 */
 	protected TypeName responseType;
 
+	/**
+	 * The name of the response variable used in the method.
+	 */
 	protected String responseVariableName = "response";
 
+	/**
+	 * @param wrapper The class of a wrapper object
+	 * @return The injector in a fluent api pattern.
+	 */
 	public ReturnStatementInjector withWrapper(TypeName wrapper) {
 		this.wrapperType = wrapper;
 		return this;
 	}
 
+	/**
+	 * @param response The class of the response object
+	 * @return The injector in a fluent api pattern.
+	 */
 	public ReturnStatementInjector withResponse(TypeName response) {
 		this.responseType = response;
 		return this;
 	}
 
+	/**
+	 * @param variable The name of the response variable.
+	 * @return The injector in a fluent api pattern.
+	 */
 	public ReturnStatementInjector withResponseVariable(String variable) {
 		this.responseVariableName = variable;
 		return this;
 	}
 
+	/**
+	 * Inject into a {@link MethodSpec.Builder} and add a return statement.
+	 *
+	 * @param builder An existing builder object that will be used.
+	 * @return The altered {@link MethodSpec.Builder}.
+	 */
 	public MethodSpec.Builder inject(MethodSpec.Builder builder) {
 		if (wrapperType != null && !wrapperType.equals(TypeName.OBJECT)) {
 			builder.returns(ParameterizedTypeName.get(ClassName.get(ResponseEntity.class), ParameterizedTypeName.get(ClassName.bestGuess(wrapperType.toString()), responseType)));
@@ -42,10 +74,20 @@ public class ReturnStatementInjector implements Injectable<MethodSpec.Builder> {
 		return builder;
 	}
 
+	/**
+	 * Add a statement that wraps the response object inside the wrapper.
+	 *
+	 * @param builder An existing builder object that will be used.
+	 */
 	protected void addWrapperContent(MethodSpec.Builder builder) {
 		builder.addStatement("wrapper.setContent(" + responseVariableName + ")");
 	}
 
+	/**
+	 * Add a statement that returns the response object.
+	 *
+	 * @param builder An existing builder object that will be used.
+	 */
 	protected void addBasicReturn(MethodSpec.Builder builder) {
 		builder.addStatement("return new $T<>(" + responseVariableName + ", $T.OK)", ResponseEntity.class, HttpStatus.class);
 	}
