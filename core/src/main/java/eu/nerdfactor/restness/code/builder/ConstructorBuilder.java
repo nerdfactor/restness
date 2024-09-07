7da4cbee97591f3ed4b41ba825a240e247b08668
Daniel Klug <mail@nerdfactor.eu>
2024-09-07 15:33:59 +0200

    Refactored ConstructorBuilder.

--- core/src/main/java/eu/nerdfactor/restness/code/builder/ConstructorBuilder.java
+++ core/src/main/java/eu/nerdfactor/restness/code/builder/ConstructorBuilder.java
@@ -10,32 +10,65 @@ import javax.lang.model.element.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A builder that creates a constructor with a list of parameters.
+ *
+ * @author Daniel Klug
+ */
 public class ConstructorBuilder implements Buildable<TypeSpec.Builder> {
 
-	List<Pair<String, TypeName>> properties = new ArrayList<>();
+	/**
+	 * A list of properties that are set by the constructor.
+	 */
+	protected final List<PropertyPair> properties = new ArrayList<>();
 
+	/**
+	 * Add a property to the constructor.
+	 *
+	 * @param name The name of the property.
+	 * @param type The {@link TypeName} of the property.
+	 * @return The builder in a fluent api pattern.
+	 */
 	public ConstructorBuilder withProperty(String name, TypeName type) {
-		this.properties.add(Pair.of(name, type));
+		this.properties.add(new PropertyPair(name, type));
 		return this;
 	}
 
-	public ConstructorBuilder withProperty(Pair<String, TypeName> prop) {
+	/**
+	 * Add a property to the constructor.
+	 *
+	 * @param prop The {@link PropertyPair} containing name and {@link TypeName} of the property.
+	 * @return The builder in a fluent api pattern.
+	 */
+	public ConstructorBuilder withProperty(PropertyPair prop) {
 		this.properties.add(prop);
 		return this;
 	}
 
+	/**
+	 * Add a property to the constructor.
+	 *
+	 * @param propertyBuilder A {@link PropertyBuilder} that provides the name and {@link TypeName} of the property.
+	 * @return The builder in a fluent api pattern.
+	 */
 	public ConstructorBuilder withProperty(PropertyBuilder propertyBuilder) {
 		return this.withProperty(propertyBuilder.getName(), propertyBuilder.getType());
 	}
 
+	/**
+	 * Create a {@link TypeSpec.Builder} containing the constructor.
+	 *
+	 * @param builder An existing builder object that will be appended.
+	 * @return The build {@link TypeSpec.Builder}.
+	 */
 	public TypeSpec.Builder buildWith(TypeSpec.Builder builder) {
 		final MethodSpec.Builder method = MethodSpec
 				.constructorBuilder()
 				.addAnnotation(Autowired.class)
 				.addModifiers(Modifier.PUBLIC);
 		this.properties.forEach(prop -> {
-			method.addParameter(prop.getSecond(), prop.getFirst());
-			method.addStatement("this." + prop.getFirst() + " = " + prop.getFirst());
+			method.addParameter(prop.type(), prop.name());
+			method.addStatement("this." + prop.name() + " = " + prop.name());
 		});
 		builder.addMethod(method.build());
 		return builder;
